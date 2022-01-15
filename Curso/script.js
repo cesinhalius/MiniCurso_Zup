fee02905c646b8dcfe1ef75d2d6a4425fb64ea8a
Cesar Augusto <cesinhalius@hotmail.com>
2022-01-14 21:12:00 -0300

    test

--- Curso/script.js
+++ Curso/script.js
@@ -1,8 +1,13 @@
-const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
+const especie = document.querySelector('#especie');
+const imagem = document.querySelector('img');
+const nomeDoPersonagem = document.querySelector('#nome');
+
+const img = document.querySelector('#imagem2');
+const estado = document.querySelector('#status');
+const species = document.querySelector('#species');
+const nameCaracter = document.querySelector('#name');
 
 
 traduzirCondicao = (data) =>{
@@ -22,6 +27,7 @@ gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
+
 pegarPersonagem = () => {
     let numeroAleatorio = gerarValorAleatorio();
     return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
@@ -36,8 +42,28 @@ pegarPersonagem = () => {
         nomeDoPersonagem.innerHTML = data.name;
         especie.innerHTML = data.species;
         condicao.innerHTML = traduzirCondicao(data);
-    });
+    })
+}
+pegarPersonagens = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        img.src = data.image;
+        img.alt = data.name;
+        nameCaracter.innerHTML = data.name;
+        species.innerHTML = data.species;
+        estado.innerHTML = traduzirCondicao(data);
+    })
 }
 
 
+
+
 botao.onclick = pegarPersonagem;
+botao.onclick = pegarPersonagens;
+ 
